6788f91e9478319d41be2eca311126ac540dd1f1
='coder101_js <='op422010@gmail.com>
2025-07-06 04:03:24 +0500

    big

---
+++ .history/ai_20250706040313.py
@@ -0,0 +1,6 @@
+ 23:02:19 up 5 days,  9:46,  4 users,  load average: 0.46, 1.22, 1.08
+root@srv888808:~# mpstat
+Linux 6.8.0-62-generic (srv888808)      07/05/25        _x86_64_        (2 CPU)
+
+23:02:32     CPU    %usr   %nice    %sys %iowait    %irq   %soft  %steal  %guest  %gnice   %idle
+23:02:32     all    1.05    0.05    0.39    0.02    0.00    0.05    0.30    0.00    0.00   98.15
